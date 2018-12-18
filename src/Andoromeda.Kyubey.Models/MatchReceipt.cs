60cd633e1ba46567ccf0e38c71e6d51f8800bc91
Yuko Zheng <1@1234.sh>
2018-12-18 10:04:54 +0000

    Updated MatchReceipt.cs

--- src/Andoromeda.Kyubey.Models/MatchReceipt.cs
+++ src/Andoromeda.Kyubey.Models/MatchReceipt.cs
@@ -22,5 +22,7 @@ namespace Andoromeda.Kyubey.Models
         public double UnitPrice { get; set; }
 
         public DateTime Time { get; set; }
+
+        public bool IsSellMatch { get; set; }
     }
 }

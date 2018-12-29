0e1a88f4bbad18fc2d9cfd70dfd117feee8c898d
Yuko Zheng <1@1234.sh>
2018-12-29 02:49:30 +0000

    Added file AlertAccount.cs

---
+++ src/Andoromeda.Kyubey.Models/AlertAccount.cs
@@ -0,0 +1,15 @@
+namespace Andoromeda.Kyubey.Models
+{
+    public class AlertAccount
+    {
+        public string Id { get; set; }
+
+        public ulong Position { get; set; }
+
+        public DateTime Begin { get; set; }
+
+        public DateTime Expire { get; set; }
+
+        public ulong SmsSent { get; set; }
+    }
+}
